7b23d720f14af6c46513692f9ee4a3f0c84e8123
Stephanie <s.nguyen6734@gmail.com>
2025-02-08 13:55:43 -0600

    first commit

---
+++ Sunsetify/Sunsetify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sunsetify
+//
+//  Created by Nguyen, Stephanie V on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
